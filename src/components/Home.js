34b7a7498199309ecadb10d5d8830ec99321c889
Emily Perkins <emilyjperkinss@Emilys-MacBook-Pro.local>
2022-11-24 13:38:54 -0700

    start

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <>
+      <div className="main-container">
+        <h1>TimeShift</h1>
+      </div>    
+    </>
+  )
+}
